ce72f9d3fdfb1c203e1b77da23f53b696f2c57a5
Marcos Oliveira <vmarcosp.pereira@gmail.com>
2022-12-11 11:50:24 -0300

    feat: aded logo

--- pages/_document.js
+++ pages/_document.js
@@ -28,13 +28,9 @@ class Document extends NextDocument {
       <Html lang="pt-BR">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
           <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Chonburi&family=Corben:wght@400;700&display=swap"
             rel="stylesheet"
           />
         </Head>
