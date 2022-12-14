9fb1120a99b9c5b6699647644e87102c0d17eb84
Marcos Oliveira <vmarcosp.pereira@gmail.com>
2022-12-14 18:25:19 -0300

    chore: added remark, remark-html, prismjs, cheerio and updated next.config

--- next.config.js
+++ next.config.js
@@ -3,12 +3,14 @@ const bsconfig = require("./bsconfig.json");
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack(config) {
+  webpack(config, { isServer }) {
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack", "url-loader"],
     });
-
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
     return config;
   },
 };
